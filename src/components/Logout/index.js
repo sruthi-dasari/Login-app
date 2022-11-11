a44bb28ce6cfa55d65602f562f2ac06934dadfe7
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2022-11-11 13:37:46 +0530

    Not able to connect files, getting errors

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -3,15 +3,19 @@ import Login from './components/Login'
 import Message from './components/Message'
 
 const Logout = () => {
+  
+    onLogout(){
+    <Login/>
+  }
 
-    onLogout = () =>{
-        <Login/>
-    }
-
-    return (
-        <Message msg = "Welcome User"/>
-        <button className = "button" onClick = "onLogout">Logout</button>
-    )
+  return (
+    <>
+      <Message msg="Welcome User" />
+      <button className="button" onClick={onLogout}>
+        Logout
+      </button>
+    </>
+  )
 }
 
 export default Logout
