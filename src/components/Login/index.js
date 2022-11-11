bcfb7f48aa7eff38a6ee83f3d0714548c7ac376b
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2022-11-11 13:36:57 +0530

    Update index.js

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,17 +1,19 @@
 import './index.css'
 import Message from './components/Message'
 
-const Login = () =>{
-    
-    onLogin = () =>{
-        <Logout/>
-    }   
-
-    return (
-    <Message msg = "Please Login"/>
-    <button className = "button" onClick = "onLogin">Login</button>
-    )
+const Login = () => {
+  onLogin(){
+    <Logout />
   }
+
+  return (
+    <>
+      <Message msg="Please Login" />
+      <button className="button" onClick={onLogin}>
+        Login
+      </button>
+    </>
+  )
 }
 
 export default Login
